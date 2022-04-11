67499be15f770c9098706f947d81dba692bc5f95
mwael2002 <102371006+mwael2002@users.noreply.github.com>
2022-04-11 18:02:31 +0200

    Update I2C_interface.h

--- Mobile_controlled_home/I2C_interface.h
+++ Mobile_controlled_home/I2C_interface.h
@@ -14,7 +14,7 @@ void I2C_Master_transmit(U8 data);
 void I2C_start(void);
 void I2C_start_inturrupt(void);
 void I2C_stop(void);
-char I2C_Master_Recieve_ACK(void);
+U8 I2C_Master_Recieve_ACK(void);
 U8 I2C_Master_Recieve_data_NACK(void);
 void I2C_Slave_init(U8 Slave_Address);
 void I2C_Listen(void);
